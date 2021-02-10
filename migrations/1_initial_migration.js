76d38f31e601a0d86d28fb8b6473c79ab85683ce
rajanpaulsarella <s.rajanpaul@gmail.com>
2021-02-10 00:44:21 -0600

    Contract migration to Celo Test Successful

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,10 @@
 const Migrations = artifacts.require("Migrations");
 
-module.exports = deployer => {
-  deployer.deploy(Migrations);
+// module.exports = deployer => {
+//   deployer.deploy(Migrations);
+// };
+
+module.exports = function (deployer, network, accounts) {
+    const userAddress = accounts[1];            // my Metamask account with Kovan ETH
+    deployer.deploy(Migrations, userAddress);
 };
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,35 @@
-var HelloWorld = artifacts.require('HelloWorld')
+// var HelloWorld = artifacts.require('HelloWorld')
+//
+// module.exports = function(deployer) {
+//   deployer.deploy(HelloWorld)
+// }
 
-module.exports = function(deployer) {
-  deployer.deploy(HelloWorld)
-}
+const PepitoDisguise = artifacts.require("PepitoDisguise");
+const Pepito = artifacts.require("Pepito");
+
+const path = require("path");
+
+const Web3 = require('web3')
+const ContractKit = require('@celo/contractkit')
+const web3 = new Web3('https://alfajores-forno.celo-testnet.org')
+const kit = ContractKit.newKitFromWeb3(web3)
+//const getAccount = require('../utils/getAccount').getAccount
+
+// async function awaitWrapper(){
+//     let account_details = await getAccount()
+//     console.log(account_details)
+//     let account = account_details.address
+//     //console.log(account)
+//     console.log(`Account address: ${account}`)
+//     kit.addAccount(account.privateKey)
+// }
+// awaitWrapper().then(module.exports).
+// console.log("before export");
+
+module.exports = function (deployer, network, account) {
+  const userAddress = account;
+  //console.log("Here" + userAddress);
+  deployer.deploy(Pepito);
+  deployer.deploy(PepitoDisguise,'0x5b3C649a533A7Fe0AcBc67De3D0CD8cde08eF808');
+  //deployer.deploy(PepitoDisguise, userAddress);
+};
